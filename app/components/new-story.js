7a1486db7033da5093e329dc02adeb741d259f88
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-29 15:17:48 -0700

    Create save1 in new-story.js

--- app/components/new-story.js
+++ app/components/new-story.js
@@ -5,6 +5,16 @@ export default Ember.Component.extend({
   actions: {
     storyFormShow)(){
       this.set('addNewStory', true);
+    },
+
+    save1(){
+      var params = {
+        headline: this.get('headline'),
+        author: this.get('author'),
+        image: this.get('image'),
+      };
+      this.set('addNewStory', false);
+      this.sendAction('save2', params);
     }
   }
 });
