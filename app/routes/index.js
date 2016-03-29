227826f7d5caf39c7f2e96950a0383c8b2a4f4cb
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-29 14:24:24 -0700

    Create stories.json, move stories array into this file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
-  var stories = [{
-    id: 1,
-    headline: "Films This Week",
-    author: "Yvonna Contreras",
-    image: "https://s-media-cache-ak0.pinimg.com/736x/08/3c/31/083c31ea32c3b6b78affba244ccddb68.jpg"
-  }, {
-    id: 2,
-    headline: "Foods This Week",
-    author: "Alexandra Rivera",
-    image: "https://lauramcdaniel64.files.wordpress.com/2015/07/food-truck-casamento-6.jpg"
-  }, {
-    id: 3,
-    headline: "Music This Week",
-    author: "Yvonna Contreras",
-    image: "http://farm5.static.flickr.com/4012/4441716857_4cfbe09ec6.jpg"
-}];
-
 export default Ember.Route.extend({
   model(){
     return stories;
-  },
+  }
 });
