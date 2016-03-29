8a3b41eb92911fbedf7d44d894456a6e9115bfcd
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-29 15:42:48 -0700

    Write update action in update-story.js

--- app/components/update-story.js
+++ app/components/update-story.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateStoryForm: false,
+  actions: {
+    updateStoryForm(){
+      this.set('updateStoryForm', true);
+    },
+    update(story){
+      var params = {
+        headline: this.get('headline'),
+        author: this.get('author'),
+        image: this.get('image'),
+      };
+      this.set('updateStoryForm', false);
+      this.sendAction('update', story, params);
+    }
+  }
 });
